9109d420c3e4679a915828450bb085fce64312b4
Guillaume Martin-Lapierre <guillaumemlapierre@gmail.com>
2023-12-18 10:28:31 -0500

    initial commit

---
+++ backend/src/main/kotlin/gmlabs/forsyth/ForsythApplication.kt
@@ -0,0 +1,11 @@
+package gmlabs.forsyth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForsythApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForsythApplication>(*args)
+}
---
+++ backend/src/test/kotlin/gmlabs/forsyth/ForsythApplicationTests.kt
@@ -0,0 +1,13 @@
+package gmlabs.forsyth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForsythApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
