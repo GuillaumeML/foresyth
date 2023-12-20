019077204b7f912e776ae7c52fff2bedd62b9b23
gml <guillaumemlapierre@gmail.com>
2023-12-20 16:48:28 -0500

    found issue with mockbean (typo: should be mockkbean) when using mockk. removed mockito from pom.xml until needed

--- backend/src/test/kotlin/gmlabs/foresyth/adapters/in/web/ChatterControllerGetAllTest.kt
+++ backend/src/test/kotlin/gmlabs/foresyth/adapters/in/web/ChatterControllerGetAllTest.kt
@@ -1,13 +1,13 @@
 
+import com.ninjasquad.springmockk.MockkBean
 import gmlabs.foresyth.ForsythApplication
 import gmlabs.foresyth.adapters.`in`.web.ChatterController
 import gmlabs.foresyth.application.GetChattersQuery
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
-import org.mockito.Mockito
+import io.mockk.every
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
@@ -22,13 +22,14 @@ class ChatterControllerGetAllTest : DescribeSpec() {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockkBean
     lateinit var getChattersQuery: GetChattersQuery
 
     init {
 
         describe("GET /chatters") {
-            Mockito.`when`(getChattersQuery.getChatters()).thenReturn(emptyList())
+            //Mockito.`when`(getChattersQuery.getChatters()).thenReturn(emptyList())
+            every { getChattersQuery.getChatters() } returns emptyList()
             it("should return 200 OK") {
                 mockMvc.perform(get("/chatters").contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk)
