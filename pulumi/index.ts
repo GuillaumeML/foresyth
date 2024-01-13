ab3053289fc01e0211802c163eb422499e61357f
Guillaume Martin-Lapierre <guillaumemlapierre@gmail.com>
2024-01-13 15:24:24 -0500

    wip testing minikube deployment

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -6,7 +6,7 @@ const isMinikube = config.requireBoolean("isMinikube");
 
 const appName = "foresyth-backend";
 const appLabels = { app: appName };
-const deployment = new k8s.apps.v1.Deployment(appName, {
+const deployment = new k8s.apps.v1.Deployment("foresyth-backend-deployment", {
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 1,
@@ -15,13 +15,16 @@ const deployment = new k8s.apps.v1.Deployment(appName, {
             spec: { containers: [{
                 name: appName,
                 image: "localhost:5000/foresyth-backend",
-                ports: [{ containerPort: 8080 }] // Make sure this matches your app's port
+                ports: [{ containerPort: 8080 }],
+                env: [
+                { name: "SPRING_PROFILES_ACTIVE", value: "nonprod" }
+                ]
             }] }
         }
     }
 });
 
-const service = new k8s.core.v1.Service(appName, {
+const service = new k8s.core.v1.Service("foresyth-backend-service", {
     metadata: { labels: deployment.spec.template.metadata.labels },
     spec: {
         type: isMinikube ? "ClusterIP" : "LoadBalancer",
@@ -29,6 +32,37 @@ const service = new k8s.core.v1.Service(appName, {
         selector: appLabels
     }
 });
+
+const postgresDeployment = new k8s.apps.v1.Deployment("postgres-deployment", {
+    spec: {
+        selector: { matchLabels: { app: "postgres" } },
+        replicas: 1,
+        template: {
+            metadata: { labels: { app: "postgres" } },
+            spec: {
+                containers: [{
+                    name: "postgres",
+                    image: "postgres:latest",
+                    env: [
+                        { name: "POSTGRES_DB", value: "postgres" },
+                        { name: "POSTGRES_USER", value: "postgres" },
+                        { name: "POSTGRES_PASSWORD", value: "postgres" }
+                    ],
+                    ports: [{ containerPort: 5432 }]
+                }],
+            },
+        },
+    },
+});
+
+ const postgresService = new k8s.core.v1.Service("postgres-service", {
+     spec: {
+         type: "ClusterIP",
+         ports: [{ port: 5432, targetPort: 5432 }],
+         selector: { app: "postgres" },
+     },
+ });
+
 
 export const ip = isMinikube
     ? service.spec.clusterIP
